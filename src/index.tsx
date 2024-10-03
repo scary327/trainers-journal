0208ab6eb08bc76fb9484b8332b05130f6969f3b
scary327 <zef1rflis@yandex.ru>
2024-10-03 14:47:06 +0500

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import {createRoot} from 'react-dom/client'
+import { App } from './Components/App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const root = document.getElementById('root')
+
+if (!root) {
+    throw new Error('root not found')
+}
+
+const container = createRoot(root)
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+]);
+
+container.render(
+    <RouterProvider router={router} />
+)
