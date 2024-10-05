daa0c9390fca6e3003297405a4a11e7dce613c42
scary327 <zef1rflis@yandex.ru>
2024-10-05 22:03:26 +0500

    fix: microfix

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
 interface IProvider {
     children: JSX.Element;
 }
 
 export const Providers: FC<IProvider> = ({ children }) => {
-    return <ErrorBoundary fallback={<div>Что-то пошло не так</div>}>{children}</ErrorBoundary>;
+    return <>{children}</>;
 };
